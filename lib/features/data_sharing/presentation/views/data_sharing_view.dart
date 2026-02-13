473f508d1b9f2239f250b096917e7e84c3bff3c7
Mo ra <omarmohamed01284@gmail.com>
2026-02-13 15:34:20 +0200

    create the feature structure and the data sharing view

---
+++ lib/features/data_sharing/presentation/views/data_sharing_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:smart_medi/core/utils/app_colors.dart';
+import 'package:smart_medi/core/widgets/app_drawer.dart';
+import 'package:smart_medi/features/data_sharing/presentation/views/widgets/data_sharing_view_body.dart';
+class DataSharingView extends StatelessWidget {
+  const DataSharingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Scaffold(
+        backgroundColor: AppColors.whiteBackgroundColor,
+        body: DataSharingViewBody(),
+        drawer: AppDrawer(selectedItem: DrawerItem.notifications),
+      ),
+    );
+  }
+}
