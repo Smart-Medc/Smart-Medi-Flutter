479958b5466224b5b9b5524ec67c60c4f596f186
Mo ra <omarmohamed01284@gmail.com>
2026-02-14 18:50:46 +0200

    complete the copy code and the code showed successfully

--- lib/features/data_sharing/presentation/views/widgets/code_generated_view_widgets/code_generated_body.dart
+++ lib/features/data_sharing/presentation/views/widgets/code_generated_view_widgets/code_generated_body.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:smart_medi/core/widgets/app_bar_back_button.dart';
 import 'package:smart_medi/core/widgets/app_header.dart';
+import 'package:smart_medi/features/data_sharing/presentation/views/widgets/code_generated_view_widgets/box_code_and_copy_buttons.dart';
+import 'package:smart_medi/features/data_sharing/presentation/views/widgets/code_generated_view_widgets/code_generated_successfully.dart';
 class CodeGeneratedBody extends StatelessWidget {
   const CodeGeneratedBody({super.key});
 
@@ -13,9 +15,12 @@ class CodeGeneratedBody extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            AppBarBackButton(),
-            AppHeader(title: 'Share Medical Records', subtitle: 'Select records to share and configure access settings'),
-
+            const AppBarBackButton(),
+            const AppHeader(title: 'Share Medical Records', subtitle: 'Select records to share and configure access settings'),
+            20.verticalSpace,
+            const CodeGeneratedSuccessfully(),
+            32.verticalSpace,
+            const BoxCodeAndCopyButtons()
           ],
         ),
       ),
