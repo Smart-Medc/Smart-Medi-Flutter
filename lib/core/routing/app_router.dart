5e5c85b61d29d9815974ea46c09977f9afc4a6d3
Mo ra <omarmohamed01284@gmail.com>
2026-02-14 19:01:17 +0200

    complete the data sharing feature

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -26,7 +26,7 @@ abstract class AppRouter {
 
   static final router = GoRouter(
     routes: [
-        GoRoute(path: '/', builder: (context, state) => const HomeView()),
+        GoRoute(path: '/', builder: (context, state) => const LoginView()),
       GoRoute(path: AppRoutes.loginView, builder: (context, state) => const LoginView()),
       GoRoute(path: AppRoutes.signUpView, builder: (context, state) => const SignUpView()),
       GoRoute(path: AppRoutes.otpVerificationView, builder: (context, state) {
--- lib/features/data_sharing/presentation/views/widgets/code_generated_view_widgets/code_generated_body.dart
+++ lib/features/data_sharing/presentation/views/widgets/code_generated_view_widgets/code_generated_body.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:smart_medi/core/routing/app_routes.dart';
 import 'package:smart_medi/core/widgets/app_bar_back_button.dart';
 import 'package:smart_medi/core/widgets/app_header.dart';
+import 'package:smart_medi/core/widgets/custom_button.dart';
 import 'package:smart_medi/features/data_sharing/presentation/views/widgets/code_generated_view_widgets/box_code_and_copy_buttons.dart';
 import 'package:smart_medi/features/data_sharing/presentation/views/widgets/code_generated_view_widgets/code_generated_successfully.dart';
+import 'package:smart_medi/features/data_sharing/presentation/views/widgets/code_generated_view_widgets/shared_records_info.dart';
+
 class CodeGeneratedBody extends StatelessWidget {
   const CodeGeneratedBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 25.w),
         child: Column(
@@ -20,7 +26,21 @@ class CodeGeneratedBody extends StatelessWidget {
             20.verticalSpace,
             const CodeGeneratedSuccessfully(),
             32.verticalSpace,
-            const BoxCodeAndCopyButtons()
+            const BoxCodeAndCopyButtons(),
+            32.verticalSpace,
+            const SharedRecordsInfo(
+              sharedRecords: [
+                'Prescription History',
+                'ECG Report',
+                'Annual Physical Examination',
+              ],
+              expirationDate: 'Dec 19, 2025',
+            ),
+            20.verticalSpace,
+            CustomButton(text: 'Done',onPressed: (){
+              GoRouter.of(context).push(AppRoutes.dataSharingView);
+            },),
+            22.verticalSpace,
           ],
         ),
       ),
