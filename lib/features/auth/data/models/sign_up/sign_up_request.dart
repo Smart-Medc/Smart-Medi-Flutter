d39f0546c84589f1c52c26e445539a0b1891095c
Mo ra <omarmohamed01284@gmail.com>
2026-02-16 18:43:20 +0200

    create the sign up request model

---
+++ lib/features/auth/data/models/sign_up/sign_up_request.dart
@@ -0,0 +1,60 @@
+class SignUpRequest {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String phoneNumber;
+  final String password;
+  final String confirmPassword;
+  final String dateOfBirth;
+  final String gender;
+  final String emergencyContactName;
+  final String emergencyContactPhone;
+  final String emergencyContactRelationship;
+
+  SignUpRequest({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.phoneNumber,
+    required this.password,
+    required this.confirmPassword,
+    required this.dateOfBirth,
+    required this.gender,
+    required this.emergencyContactName,
+    required this.emergencyContactPhone,
+    required this.emergencyContactRelationship,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'firstName': firstName,
+      'lastName': lastName,
+      'email': email,
+      'phoneNumber': phoneNumber,
+      'password': password,
+      'confirmPassword': confirmPassword,
+      'dateOfBirth': dateOfBirth,
+      'gender': gender,
+      'emergencyContactName': emergencyContactName,
+      'emergencyContactPhone': emergencyContactPhone,
+      'emergencyContactRelationship': emergencyContactRelationship,
+    };
+  }
+
+  factory SignUpRequest.fromJson(Map<String, dynamic> json) {
+    return SignUpRequest(
+      firstName: json['firstName'] as String,
+      lastName: json['lastName'] as String,
+      email: json['email'] as String,
+      phoneNumber: json['phoneNumber'] as String,
+      password: json['password'] as String,
+      confirmPassword: json['confirmPassword'] as String,
+      dateOfBirth: json['dateOfBirth'] as String,
+      gender: json['gender'] as String,
+      emergencyContactName: json['emergencyContactName'] as String,
+      emergencyContactPhone: json['emergencyContactPhone'] as String,
+      emergencyContactRelationship: json['emergencyContactRelationship'] as String,
+    );
+  }
+}
+
