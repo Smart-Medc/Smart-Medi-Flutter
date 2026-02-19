cf526c796ca0ea499c0954fafd476cc1fcd43e1d
Mo ra <omarmohamed01284@gmail.com>
2026-02-19 16:03:01 +0200

    add login functionality with request model and implementation

--- lib/features/auth/data/repos/auth_repo/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo/auth_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:dio/dio.dart';
 import 'package:smart_medi/core/networking/api_endpoints.dart';
 import 'package:smart_medi/core/networking/api_failure.dart';
 import 'package:smart_medi/core/networking/api_service.dart';
+import 'package:smart_medi/features/auth/data/models/login/login_request.dart';
 import 'package:smart_medi/features/auth/data/models/login/login_response.dart';
 import 'package:smart_medi/features/auth/data/models/sign_up/sign_up_request_model.dart';
 import 'package:smart_medi/features/auth/data/models/verify_email/verify_email_request.dart';
@@ -30,6 +31,21 @@ class AuthRepoImpl extends AuthRepo{
   Future<Either<Failure, LoginResponse>> verifyEmail({required VerifyEmailRequest verifyEmailRequest}) async{
     try {
       final data = await apiService.post(endpoint: ApiEndpoints.verifyEmail, data: verifyEmailRequest.toJson());
+      final loginResponse = LoginResponse.fromJson(data);
+      return right(loginResponse);
+    } catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      } else {
+        return left(ServerFailure(e.toString()));
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, LoginResponse>> login({required LoginRequest loginRequest}) async{
+    try {
+      final data = await apiService.post(endpoint: ApiEndpoints.login, data: loginRequest.toJson());
       final loginResponse = LoginResponse.fromJson(data);
       return right(loginResponse);
     } catch (e) {
