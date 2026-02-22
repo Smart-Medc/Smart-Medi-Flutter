af4ec677cce439e14b5de1dc1442b6255ad27a1a
Mo ra <omarmohamed01284@gmail.com>
2026-02-22 13:34:00 +0200

    feat: add password recovery functionality with email verification and reset capabilities (add the password recovery flow repo)

---
+++ lib/features/auth/data/repos/password_recovery_repo/password_recovery_repo.dart
@@ -0,0 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:smart_medi/core/networking/api_failure.dart';
+import 'package:smart_medi/features/auth/data/models/email_request/email_request.dart';
+import 'package:smart_medi/features/auth/data/models/reset_password/reset_password_request.dart';
+import 'package:smart_medi/features/auth/data/models/verify_reset_code/verify_reset_code_request.dart';
+
+abstract class PasswordRecoveryRepo {
+  Future<Either<Failure, Unit>> forgotPassword({required EmailRequest emailRequest});
+  Future<Either<Failure, Unit>> verifyResetCode({required VerifyResetCodeRequest verifyResetCodeRequest});
+  Future<Either<Failure, Unit>> resetPassword({required ResetPasswordRequest resetPasswordRequest});
+}
