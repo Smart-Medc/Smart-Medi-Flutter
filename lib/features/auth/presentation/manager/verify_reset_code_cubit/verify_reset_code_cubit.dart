21ad928952983fb4c3b42df233eba87e1856a3b9
Mo ra <omarmohamed01284@gmail.com>
2026-02-22 14:09:43 +0200

    feat: add VerifyResetCodeCubit and state management for reset code verification

---
+++ lib/features/auth/presentation/manager/verify_reset_code_cubit/verify_reset_code_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:smart_medi/features/auth/data/models/verify_reset_code/verify_reset_code_request.dart';
+import 'package:smart_medi/features/auth/data/repos/password_recovery_repo/password_recovery_repo.dart';
+
+part 'verify_reset_code_state.dart';
+
+class VerifyResetCodeCubit extends Cubit<VerifyResetCodeState> {
+  VerifyResetCodeCubit(this.passwordRecoveryRepo) : super(VerifyResetCodeInitial());
+  final PasswordRecoveryRepo passwordRecoveryRepo;
+
+  Future<void> verifyResetCode({required VerifyResetCodeRequest verifyCodeResetRequest}) async {
+    emit(VerifyResetCodeLoading());
+    final result = await passwordRecoveryRepo.verifyResetCode(verifyResetCodeRequest: verifyCodeResetRequest);
+    result.fold(
+          (failure) => emit(VerifyResetCodeFailure(message: failure.message)),
+          (_) => emit(VerifyResetCodeSuccess()),
+    );
+  }
+}
---
+++ lib/features/auth/presentation/manager/verify_reset_code_cubit/verify_reset_code_state.dart
@@ -0,0 +1,13 @@
+part of 'verify_reset_code_cubit.dart';
+
+@immutable
+sealed class VerifyResetCodeState {}
+
+final class VerifyResetCodeInitial extends VerifyResetCodeState {}
+final class VerifyResetCodeLoading extends VerifyResetCodeState {}
+final class VerifyResetCodeSuccess extends VerifyResetCodeState {}
+final class VerifyResetCodeFailure extends VerifyResetCodeState {
+  VerifyResetCodeFailure({required this.message});
+
+  final String message;
+}
