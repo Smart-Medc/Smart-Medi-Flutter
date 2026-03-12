3c8581d63398a9aff7c9931de918b9131cba9c04
Mo ra <omarmohamed01284@gmail.com>
2026-03-12 17:09:40 +0200

    feat: replace AppHeader with HomeHeader for dynamic user greeting and load username from secure storage

--- lib/features/home/presentation/views/widgets/home_body.dart
+++ lib/features/home/presentation/views/widgets/home_body.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:smart_medi/core/utils/app_styles.dart';
-import 'package:smart_medi/core/widgets/app_header.dart';
 import 'package:smart_medi/core/widgets/app_search_bar.dart';
 import 'package:smart_medi/core/widgets/custom_app_bar.dart';
+import 'package:smart_medi/features/home/presentation/views/widgets/home_header.dart';
 import 'package:smart_medi/features/home/presentation/views/widgets/home_view_upcoming.dart';
 import 'package:smart_medi/features/home/presentation/views/widgets/quick_actions_list_view.dart';
 import 'package:smart_medi/features/home/presentation/views/widgets/home_view_recent_records.dart';
@@ -24,10 +24,7 @@ class HomeBody extends StatelessWidget {
               padding: EdgeInsets.only(right: 25.w),
               child: const CustomAppBar(),
             ),
-            const AppHeader(
-              title: 'Hi, Omar!',
-              subtitle: 'How are you feeling today?',
-            ),
+            const HomeHeader(),
             Padding(
               padding: EdgeInsets.only(right: 25.w),
               child: const AppSearchBar(hintText: 'Search doctors'),
