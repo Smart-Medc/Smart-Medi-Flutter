ba34c320a4db724af91be9bbed153917eb969f31
Mo ra <omarmohamed01284@gmail.com>
2026-03-14 16:36:31 +0200

    feat: implement AddMedicationRequest class and define MedicationRepo interface for medication management integration

---
+++ lib/features/medication_management/data/models/add_medication_request.dart
@@ -0,0 +1,39 @@
+class AddMedicationRequest {
+
+  AddMedicationRequest({
+    required this.name,
+    required this.dosage,
+    required this.frequency,
+    required this.route,
+    required this.instructions,
+    required this.startDate,
+    required this.endDate,
+    required this.prescribingDoctor,
+  }) {
+    if (endDate.isBefore(startDate)) {
+      throw ArgumentError('End date must be after start date');
+    }
+  }
+  final String name;
+  final String dosage;
+  final String frequency;
+  final String route;
+  final String instructions;
+  final DateTime startDate;
+  final DateTime endDate;
+  final String prescribingDoctor;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'dosage': dosage,
+      'frequency': frequency,
+      'route': route,
+      'instructions': instructions,
+      'startDate': startDate.toIso8601String(),
+      'endDate': endDate.toIso8601String(),
+      'prescribingDoctor': prescribingDoctor,
+    };
+  }
+}
+
---
+++ lib/features/medication_management/data/repos/medication_repo_impl.dart
@@ -0,0 +1,6 @@
+import 'package:smart_medi/features/medication_management/data/repos/medication_repo.dart';
+
+class MedicationRepoImpl extends MedicationRepo {
+
+}
+
