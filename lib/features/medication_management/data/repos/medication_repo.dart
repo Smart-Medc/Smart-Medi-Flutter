1767cf382871fa7886e2e148323f56ba81ffea7d
Mo ra <omarmohamed01284@gmail.com>
2026-03-14 16:49:30 +0200

    feat: implement addMedication method in MedicationRepoImpl for medication management integration

--- lib/features/medication_management/data/repos/medication_repo.dart
+++ lib/features/medication_management/data/repos/medication_repo.dart
@@ -1,4 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:smart_medi/core/networking/api_failure.dart';
+import 'package:smart_medi/features/medication_management/data/models/add_medication_request.dart';
+
 abstract class MedicationRepo {
-
+  Future<Either<Failure, Unit>> addMedication({
+    required AddMedicationRequest addMedicationRequest,
+    required String patientId,
+  });
 }
-
--- lib/features/medication_management/data/repos/medication_repo_impl.dart
+++ lib/features/medication_management/data/repos/medication_repo_impl.dart
@@ -1,6 +1,27 @@
+import 'package:dartz/dartz.dart';
+import 'package:smart_medi/core/helpers/api_helper.dart';
+import 'package:smart_medi/core/networking/api_endpoints.dart';
+import 'package:smart_medi/core/networking/api_failure.dart';
+import 'package:smart_medi/core/networking/api_service.dart';
+import 'package:smart_medi/features/medication_management/data/models/add_medication_request.dart';
 import 'package:smart_medi/features/medication_management/data/repos/medication_repo.dart';
 
 class MedicationRepoImpl extends MedicationRepo {
+  MedicationRepoImpl({required this.apiService});
 
+  final ApiService apiService;
+
+  @override
+  Future<Either<Failure, Unit>> addMedication({
+    required AddMedicationRequest addMedicationRequest,
+    required String patientId,
+  }) async {
+    return ApiHelper.execute<Unit>(() async {
+      await apiService.post(
+        endpoint: ApiEndpoints.addPatientMedication(patientId: patientId),
+        data: addMedicationRequest.toJson(),
+      );
+      return unit;
+    });
+  }
 }
-
