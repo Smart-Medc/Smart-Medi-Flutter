78f6be98b30f613332de7ede9aa4315a340bd50c
Mo ra <omarmohamed01284@gmail.com>
2026-03-23 19:56:13 +0200

    feat: integrate BlocProvider for GetMedicalJournalsCubit in MedicalJournalView

--- lib/features/medical_journal/presentation/views/medical_journal_view.dart
+++ lib/features/medical_journal/presentation/views/medical_journal_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smart_medi/core/helpers/service_locator.dart';
 import 'package:smart_medi/core/utils/app_colors.dart';
 import 'package:smart_medi/core/widgets/app_drawer.dart';
+import 'package:smart_medi/features/medical_journal/data/repos/medical_journal_repo.dart';
+import 'package:smart_medi/features/medical_journal/presentation/manager/get_medical_journals/get_medical_journals_cubit.dart';
 import 'package:smart_medi/features/medical_journal/presentation/views/widgets/medical_journal_widgets/medical_journal_view_body.dart';
 
 class MedicalJournalView extends StatelessWidget {
@@ -8,11 +12,15 @@ class MedicalJournalView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
         backgroundColor: AppColors.whiteBackgroundColor,
-        body: MedicalJournalViewBody(),
-        drawer: AppDrawer(selectedItem: DrawerItem.journal),
+        body: BlocProvider(
+          create: (context) =>
+              GetMedicalJournalsCubit(getIt<MedicalJournalRepo>())..getMedicalJournalsForCurrentPatient(),
+          child: const MedicalJournalViewBody(),
+        ),
+        drawer: const AppDrawer(selectedItem: DrawerItem.journal),
       ),
     );
   }
