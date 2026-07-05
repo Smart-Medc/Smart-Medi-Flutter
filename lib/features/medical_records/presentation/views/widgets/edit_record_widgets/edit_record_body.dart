36927e240701641b4610ea455bf0b305e95fd5e1
Mo ra <omarmohamed01284@gmail.com>
2026-07-05 21:14:16 +0300

    complete adding the record without any documents

--- lib/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_body.dart
+++ lib/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_body.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:smart_medi/core/widgets/app_bar_back_button.dart';
 import 'package:smart_medi/core/widgets/app_header.dart';
-import 'package:smart_medi/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_basic_content.dart';
+import 'package:smart_medi/features/medical_records/presentation/views/widgets/edit_record_widgets/add_medical_record_bloc_listener.dart';
 
 class EditRecordBody extends StatelessWidget {
   const EditRecordBody({super.key, required this.isEdit});
@@ -22,7 +22,7 @@ class EditRecordBody extends StatelessWidget {
               subtitle: isEdit ? 'Update your medical record information' : 'Fill in the details for your new medical record',
             ),
             24.verticalSpace,
-            EditRecordBasicContent(isEdit: isEdit),
+            AddMedicalRecordBlocListener(isEdit: isEdit),
           ],
         ),
       ),
--- lib/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_content.dart
+++ lib/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_content.dart
@@ -1,30 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:smart_medi/core/helpers/validator.dart';
 import 'package:smart_medi/core/utils/app_colors.dart';
 import 'package:smart_medi/core/utils/app_styles.dart';
 import 'package:smart_medi/core/widgets/card_container.dart';
 import 'package:smart_medi/core/widgets/custom_text_form_field.dart';
+import 'package:smart_medi/features/medical_records/data/models/add_medical_record_models/add_medical_record_request.dart';
+import 'package:smart_medi/features/medical_records/presentation/manager/add_medical_record_cubit/add_medical_record_cubit.dart';
 import 'package:smart_medi/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_action_buttons.dart';
 import 'package:smart_medi/features/medical_records/presentation/views/widgets/upload_new_document.dart';
 
-class EditRecordBasicContent extends StatefulWidget {
-  const EditRecordBasicContent({super.key, required this.isEdit});
+class EditRecordContent extends StatefulWidget {
+  const EditRecordContent({super.key, required this.isEdit});
+
   final bool isEdit;
+
   @override
-  State<EditRecordBasicContent> createState() => _EditRecordBasicContentState();
+  State<EditRecordContent> createState() => _EditRecordContentState();
 }
 
-class _EditRecordBasicContentState extends State<EditRecordBasicContent> {
+class _EditRecordContentState extends State<EditRecordContent> {
   final TextEditingController _recordTitleController = TextEditingController();
-  final TextEditingController _recordTypeController = TextEditingController(text: 'Imaging');
+  final TextEditingController _recordTypeController = TextEditingController(
+    text: 'Imaging',
+  );
   final TextEditingController _startDateController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
   final TextEditingController _providerNameController = TextEditingController();
   final TextEditingController _orderedByController = TextEditingController();
-  final TextEditingController _findingSummaryController = TextEditingController();
+  final TextEditingController _findingSummaryController =
+      TextEditingController();
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-
 
   final List<String> _recordTypes = [
     'Lab Report',
@@ -60,7 +67,10 @@ class _EditRecordBasicContentState extends State<EditRecordBasicContent> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text('Basic Information', style: AppStyles.textStyle24W600Black),
+                Text(
+                  'Basic Information',
+                  style: AppStyles.textStyle24W600Black,
+                ),
                 16.verticalSpace,
 
                 // Record Title
@@ -68,7 +78,10 @@ class _EditRecordBasicContentState extends State<EditRecordBasicContent> {
                 8.verticalSpace,
                 CustomTextFormField(
                   controller: _recordTitleController,
-                  validator: (value) => Validator.requiredValidator(value, 'Please enter a record title'),
+                  validator: (value) => Validator.requiredValidator(
+                    value,
+                    'Please enter a record title',
+                  ),
                   hintText: 'Chest X-Ray',
                 ),
                 16.verticalSpace,
@@ -78,7 +91,10 @@ class _EditRecordBasicContentState extends State<EditRecordBasicContent> {
                 8.verticalSpace,
                 CustomTextFormField.dropdown(
                   controller: _recordTypeController,
-                  validator: (value) => Validator.requiredValidator(value, 'Please select a record type'),
+                  validator: (value) => Validator.requiredValidator(
+                    value,
+                    'Please select a record type',
+                  ),
                   hintText: 'Select record type',
                   dropdownItems: _recordTypes,
                 ),
@@ -89,7 +105,10 @@ class _EditRecordBasicContentState extends State<EditRecordBasicContent> {
                 8.verticalSpace,
                 CustomTextFormField.date(
                   controller: _startDateController,
-                  validator: (value) => Validator.requiredValidator(value, 'Please select a start date'),
+                  validator: (value) => Validator.requiredValidator(
+                    value,
+                    'Please select a start date',
+                  ),
                   hintText: '03/10/2024',
                 ),
                 16.verticalSpace,
@@ -119,7 +138,8 @@ class _EditRecordBasicContentState extends State<EditRecordBasicContent> {
                   controller: _descriptionController,
                   maxLines: 4,
                   decoration: InputDecoration(
-                    hintText: 'Chest X-ray imaging study for routine health screening.',
+                    hintText:
+                        'Chest X-ray imaging study for routine health screening.',
                     hintStyle: AppStyles.textStyle12W400DarkGrey,
                     filled: true,
                     fillColor: AppColors.formFieldBGColor,
@@ -148,7 +168,8 @@ class _EditRecordBasicContentState extends State<EditRecordBasicContent> {
                   controller: _findingSummaryController,
                   maxLines: 2,
                   decoration: InputDecoration(
-                    hintText: 'Findings summary for the chest X-ray imaging study.',
+                    hintText:
+                        'Findings summary for the chest X-ray imaging study.',
                     hintStyle: AppStyles.textStyle12W400DarkGrey,
                     filled: true,
                     fillColor: AppColors.formFieldBGColor,
@@ -168,20 +189,44 @@ class _EditRecordBasicContentState extends State<EditRecordBasicContent> {
                     ),
                   ),
                 ),
-
               ],
-
             ),
           ),
           16.verticalSpace,
-          const SizedBox(width: double.infinity,child: UploadNewDocument()),
+          const SizedBox(width: double.infinity, child: UploadNewDocument()),
           40.verticalSpace,
-           EditRecordActionButtons(
-             isEdit: widget.isEdit,
-             onPrimaryPressed: (){
-             if (_formKey.currentState!.validate()) {
-             }
-          },),
+          EditRecordActionButtons(
+            isEdit: widget.isEdit,
+            onPrimaryPressed: () {
+              if (_formKey.currentState!.validate()) {
+                final request = AddMedicalRecordRequest(
+                  title: _recordTitleController.text.trim(),
+                  recordDate: DateTime.parse(_startDateController.text),
+                  recordType: _recordTypeController.text.trim() == 'Consultation Notes' ? 'ConsultationNotes' : _recordTypeController.text.trim(),
+
+                  providerName: _providerNameController.text.trim().isEmpty
+                      ? null
+                      : _providerNameController.text.trim(),
+
+                  orderedBy: _orderedByController.text.trim().isEmpty
+                      ? null
+                      : _orderedByController.text.trim(),
+
+                  description: _descriptionController.text.trim().isEmpty
+                      ? null
+                      : _descriptionController.text.trim(),
+
+                  findingsSummary: _findingSummaryController.text.trim().isEmpty
+                      ? null
+                      : _findingSummaryController.text.trim(),
+                );
+
+                context.read<AddMedicalRecordCubit>().addMedicalRecord(
+                  addMedicalRecordRequest: request,
+                );
+              }
+            },
+          ),
           60.verticalSpace,
         ],
       ),
@@ -207,4 +252,3 @@ class _EditRecordBasicContentState extends State<EditRecordBasicContent> {
     );
   }
 }
-
