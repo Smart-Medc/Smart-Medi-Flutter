de58e059aee9fc263de6f4e56780484bf3e67e5b
Mo ra <omarmohamed01284@gmail.com>
2026-07-05 20:27:08 +0300

    prepare the upload document view to integration with API

--- lib/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_action_buttons.dart
+++ lib/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_action_buttons.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:smart_medi/core/widgets/action_buttons.dart';
 
 class EditRecordActionButtons extends StatelessWidget {
-  const EditRecordActionButtons({super.key});
+  const EditRecordActionButtons({super.key, this.onPrimaryPressed});
+  final void Function()? onPrimaryPressed;
 
   @override
   Widget build(BuildContext context) {
-    return const ActionButtons(
+    return ActionButtons(
       primaryButtonText: 'Save Changes',
+      onPrimaryPressed: onPrimaryPressed,
     );
   }
 }
--- lib/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_body.dart
+++ lib/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_body.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:smart_medi/core/widgets/app_bar_back_button.dart';
 import 'package:smart_medi/core/widgets/app_header.dart';
-import 'package:smart_medi/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_action_buttons.dart';
-import 'package:smart_medi/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_basic_information.dart';
-import 'package:smart_medi/features/medical_records/presentation/views/widgets/upload_new_document.dart';
+import 'package:smart_medi/features/medical_records/presentation/views/widgets/edit_record_widgets/edit_record_basic_content.dart';
 
 class EditRecordBody extends StatelessWidget {
   const EditRecordBody({super.key});
@@ -24,12 +22,7 @@ class EditRecordBody extends StatelessWidget {
               subtitle: 'Update your medical record information',
             ),
             24.verticalSpace,
-            const EditRecordBasicInformation(),
-            16.verticalSpace,
-            const SizedBox(width: double.infinity,child: UploadNewDocument()),
-            40.verticalSpace,
-            const EditRecordActionButtons(),
-            60.verticalSpace,
+            const EditRecordBasicContent(),
           ],
         ),
       ),
